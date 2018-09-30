86879bd48018e45779795b5364fef8eead397e9e
Anant <anant02sep@gmail.com>
2018-09-30 15:14:00 +0000

    thresholding

--- src/DetectTextLines.py
+++ src/DetectTextLines.py
@@ -2,10 +2,45 @@ import numpy as np
 import cv2
 import math
 import sys
-import Util
+from Util import *
 
 # Print full numpy array
-np.set_printoptions(threshold=np.nan)
+# np.set_printoptio	ns(threshold=np.nan)
 
-input_img_path="document.png"
-input_image = cv2.imread(input_img_path,0)
+input_img_path="../resources/document.png"
+input_img = cv2.imread(input_img_path,0)
+
+bin_img = binarize(input_img, 13, 8)
+show(bin_img)
+
+# show(invert(bin_img))
+
+num_labels,labels,stats,centroids = connected_components(bin_img)
+# print(num_labels)
+# print(centroids)
+
+
+# DAFAQ is this
+# def getmarkerboundingrect(img, mkpos, mksize):
+#     buffer = int(mksize * 0.15)
+#     x = mkpos[0] - buffer
+#     y = mkpos[1] - buffer
+#     w = mksize + buffer*2
+#     h = mksize + buffer*2
+#     roi = img[y:y+h, x:x+w]
+    
+#     # grayroi = getgrayimage(roi)
+#     # ret, binimage = cv2.threshold(grayroi,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+#     binimage = binarize(img, 13, 8)
+#     nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(binimage)
+#     # stats[0], centroids[0] are for the background label. ignore
+#     # cv2.CC_STAT_LEFT, cv2.CC_STAT_TOP, cv2.CC_STAT_WIDTH, cv2.CC_STAT_HEIGHT
+#     lblareas = stats[1:,cv2.CC_STAT_AREA]
+#     imax = max(enumerate(lblareas), key=(lambda x: x[1]))[0] + 1
+#     boundingrect = Rect(stats[imax, cv2.CC_STAT_LEFT],
+#                         stats[imax, cv2.CC_STAT_TOP], 
+#                         stats[imax, cv2.CC_STAT_WIDTH], 
+#                         stats[imax, cv2.CC_STAT_HEIGHT])
+#     return boundingrect.addoffset((x,y)) 
+
+# print(getmarkerboundingrect(bin_img,[0,0],1))
--- src/Util.py
+++ src/Util.py
@@ -6,3 +6,12 @@ import math
 def show(image,name="image"):
     cv2.imshow(name,image)
     cv2.waitKey(0)
+
+def binarize(img, a, b):
+	return cv2.adaptiveThreshold(img,255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,a,b)
+
+def invert(img):
+	return cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+
+def connected_components(img):
+	return cv2.connectedComponentsWithStats(img, connectivity=8)
